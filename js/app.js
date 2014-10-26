bded51ce9c46c7190c3ca33f6c9068dc070812ed
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-10-26 11:34:25 -0700

    returned the add product button sidebar

--- js/app.js
+++ js/app.js
@@ -39,6 +39,18 @@ $( document ).ready(function() {
      }
     });
   
+$('.cd-btn').on('click', function(event){
+    event.preventDefault();
+    $('.cd-panel').addClass('is-visible');
+});
+//clode the lateral panel
+$('.cd-panel').on('click', function(event){
+    if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+        $('.cd-panel').removeClass('is-visible');
+        event.preventDefault();
+    }
+});
+
 function compareNumbers(a, b) {
   return b.score - a.score;
 }
