f167872bd2e5a5cee782ab7b99ac722c2cac3b68
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2014-10-26 09:20:43 -0700

    add services

--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
-var vivahunt = angular.module('vivahunt',[])
-.controller('mainController', function ($scope) {
-  $scope.user = {
-    picture : "http://api.randomuser.me/portraits/med/women/33.jpg"
-  };
+$( document ).ready(function() {
 
-  $scope.topics = [
-  { text : 'startup' , link : 'startup'},
-  { text : 'moda' , link : 'moda'},
-  { text : 'culinária' , link : 'culinaria'},
-  { text : 'música' , link : 'musica'},
-  { text : 'cinema' , link : 'cinema'},
-  { text : 'art & design' , link : 'design'},
-  { text : 'arquitetura' , link : 'arquitetura'},
-  ];
-    
-    
-  $scope.animateButton = function(){
-  
-    $('.cd-btn').on('click', function(event){
-        event.preventDefault();
-        $('.cd-panel').addClass('is-visible');
-    });
-    //clode the lateral panel
-    $('.cd-panel').on('click', function(event){
-        if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
-            $('.cd-panel').removeClass('is-visible');
-            event.preventDefault();
-        }
+  var vivaAPI = "http://vivahunt.com/";
+  $.ajax({
+     type : "GET",
+     dataType : "json",
+     url : vivaAPI + "posts/today",
+     success: function(data){
+          console.log(data);
+        $.each( data.posts, function( i, item ) {
+        $( "#postslinks ul").append('<li style="clear:both"><div class="col-xs-2 upvote"><button>' + item.score + '</button></div>' + '<div class="col-xs-7 desc"><h2><a href="#/" target="_blank">' + item.name + '</a></h2><p>' + item.tagline + '</p></div>' + '<div class="col-xs-3 user"><img src="http://api.randomuser.me/portraits/med/women/'+ i + '.jpg" ></div></li>');
       });
+     }
+    });
+    console.log(vivaAPI);
+    $.ajax({
+     type : "GET",
+     dataType : "json",
+     url : vivaAPI + "categories",
+     success: function(data){
+          console.log(data);
+        $.each( data.categories, function( i, item ) {
+        $( "ul.topics").append('<li>' + item.category + '</li>');
+      });
+     }
+    });
 
-  }
+
 
 });
     
